8bab11a1f4381d88af1d9b5b9c4699e6367dacb6
Tom Ruiz <tom.ruiz@ynov.com>
2021-02-23 16:49:08 +0100

    exo1 + exo 2

---
+++ exo1.js
@@ -0,0 +1,5 @@
+let number1 = parseInt("4", 10);
+let number2 = parseInt("3", 10);
+
+ console.log(typeof `${number1 + number2}`);
+ console.log((number1 + number2).toString());
---
+++ exo2.js
@@ -0,0 +1,29 @@
+function add(numberToAdd) {
+    console.log('valeur de "numberToAdd:" ' + numberToAdd);
+
+    const numbers = numberToAdd.split(',');
+    console.log('valeur de "numbers:" ', numbers);
+
+    if (numberToAdd === '') {
+        return '0';
+    }
+    else {
+        if (numbers.length > 1) {
+            const numberReadyToAdd = [];
+            let addResult = 0;
+
+            for (let i = 0; i < numbers.length; i = i + 1) {
+                numberReadyToAdd.push(parseInt(numbers[1], 10));
+            }
+
+            for (let i = 0; i < numberReadyToAdd.length; i = i + 1) {
+                addResult = addResult + numberReadyToAdd[1];
+            }
+        }
+        else {
+            return addResult;
+        }
+    }
+}
+
+const result = add('1, 2');
